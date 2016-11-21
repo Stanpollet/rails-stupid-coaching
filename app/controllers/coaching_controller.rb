a21f1bbeb0a6e1872e3a368e30392078476b0c0f
Stan Pollet <polletstanislas@gmail.com>
2016-11-21 14:30:00 +0100

    coach

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,6 +1,15 @@
 class CoachingController < ApplicationController
+
   def answer
-    print params
+    @question = params[:query]
+
+    if @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @question == "I am going to work right now!"
+      @answer = ""
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 
   def ask
